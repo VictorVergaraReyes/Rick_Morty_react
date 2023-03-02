231c64bad294d29e945d11d288510a324414f204
Victor <victorvergarareyes2002@gmail.com>
2023-03-01 20:23:28 -0600

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://VictorVergaraReyes.github.io/Rick_Morty_react/",
+  base:"/Rick_Morty_react/",
   plugins: [react()],
 })
